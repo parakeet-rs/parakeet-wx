2abb7185cc58c5f6bfa89f6b8cda5bc38410a992
Jixun Wu <jixun.moe@gmail.com>
2022-05-01 00:46:11 +0100

    crypto(common): added StreamCipher interface

---
+++ um-crypto/include/um-crypto/common.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+namespace umc {
+
+// type aliases
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using usize = size_t;
+#if UINTPTR_MAX == UINT64_MAX
+using isize = i64;
+#else
+using isize = i32;
+#endif
+
+template <class T>
+using Vec = std::vector<T>;
+
+}  // namespace umc
---
+++ um-crypto/include/um-crypto/interface/IStreamCipher.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "um-crypto/common.h"
+
+namespace umc {
+
+class IStreamCipher {
+ public:
+  IStreamCipher(){};
+  virtual ~IStreamCipher(){};
+
+  virtual bool Encrypt(Vec<u8>& result, const Vec<u8>& input) = 0;
+  virtual bool Decrypt(Vec<u8>& result, const Vec<u8>& input) = 0;
+  virtual void Seek(usize offset) { this->offset = offset; }
+
+ protected:
+  usize offset;
+};
+
+}  // namespace umc
