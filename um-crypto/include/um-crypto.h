db9e409c2d1a7b0b40808fd6c4cd547991a3952e
Jixun Wu <jixun.moe@gmail.com>
2022-05-01 15:16:23 +0100

    dev: header clean up

--- um-crypto/include/um-crypto.h
+++ um-crypto/include/um-crypto.h
@@ -1,12 +1,4 @@
 #pragma once
 
-// Algorithm implementation
-#include "um-crypto/tc_tea.h"
-
-// Decryptors
-// Kugou
-#include "um-crypto/kgm.h"
-#include "um-crypto/vpr.h"
-
-// QQ Music
+#include "um-crypto/kugou.h"
 #include "um-crypto/qmcv2.h"
