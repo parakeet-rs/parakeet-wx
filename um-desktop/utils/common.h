040acbaef212e9eb634625ee77114fa26ed910cd
Jixun <jixun.moe@gmail.com>
2022-05-02 22:32:53 +0100

    chore: re-export types from um-crypto

---
+++ um-desktop/utils/common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <um-crypto/common.h>
+
+namespace umd {
+
+// Re-import types
+using umc::u16;
+using umc::u32;
+using umc::u64;
+using umc::u8;
+using umc::usize;
+
+using umc::i16;
+using umc::i32;
+using umc::i64;
+using umc::i8;
+using umc::isize;
+
+using umc::Str;
+using umc::Vec;
+
+}  // namespace umd
