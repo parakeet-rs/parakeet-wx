06b4e444284e5fed542517322022aad83447d346
Jixun <jixun.moe@gmail.com>
2022-05-03 01:47:51 +0100

    ci: build localosation file

---
+++ build-po.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+find locale -name '*.po' -type f -print0 |
+  while read -r -d '' po_file; do
+    mo_file="$(echo "$po_file" | sed 's/po$/mo/')"
+    msgfmt -o "$mo_file" "$po_file"
+  done
