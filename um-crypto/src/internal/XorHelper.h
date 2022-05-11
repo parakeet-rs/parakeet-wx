95821ce2bd014dbbc6088b446a9f424a56e14fc8
Jixun Wu <jixun.moe@gmail.com>
2022-05-11 19:05:23 +0100

    chore: clean up and document xor_block helpers

--- um-crypto/src/internal/XorHelper.h
+++ um-crypto/src/internal/XorHelper.h
@@ -4,30 +4,82 @@
 
 namespace umc {
 
-inline void XorBlock(u8* p_out, const u8* key, usize len) {
+/**
+ * @brief XOR operation of two blocks.
+ *
+ * @param p_in_out
+ * @param p_key
+ * @param len
+ */
+inline void XorBlock(void* p_in_out, const void* p_key, usize len) {
   for (usize i = 0; i < len; i++) {
-    p_out[i] ^= key[i];
+    reinterpret_cast<u8*>(p_in_out)[i] ^= reinterpret_cast<const u8*>(p_key)[i];
   }
 }
 
-inline void XorBlock(u8* p_out, const u8* p_in, const u8* key, usize len) {
+/**
+ * @brief XOR operation of two blocks (in1 & in2),
+ *        then store its result to p_out.
+ *
+ * @param p_out
+ * @param p_in1
+ * @param p_in2
+ * @param len
+ */
+inline void XorBlock(void* p_out,
+                     const void* p_in1,
+                     const void* p_in2,
+                     usize len) {
   for (usize i = 0; i < len; i++) {
-    p_out[i] = p_in[i] ^ key[i];
+    reinterpret_cast<u8*>(p_out)[i] = reinterpret_cast<const u8*>(p_in1)[i] ^
+                                      reinterpret_cast<const u8*>(p_in2)[i];
   }
 }
 
-inline void XorBlock(u8* p_out,
+/**
+ * @brief XOR operation of a data block and key block.
+ *        Once key offset had reached the end, it will reset to index 0.
+ *
+ * @param p_in_out
+ * @param out_len
+ * @param key
+ * @param key_len
+ * @param key_offset
+ */
+inline void XorBlock(void* p_in_out,
                      usize out_len,
-                     const u8* key,
+                     const void* key,
                      usize key_len,
-                     usize offset) {
-  usize j = offset % key_len;
+                     usize key_offset) {
+  const usize j = key_offset % key_len;
   for (usize i = 0; i < out_len; i++) {
-    p_out[i] ^= key[j++];
+    reinterpret_cast<u8*>(p_in_out)[i] ^=
+        reinterpret_cast<const u8*>(key)[(j + i) % key_len];
+  }
+}
 
-    if (j == key_len) {
-      j = 0;
-    }
+/**
+ * @brief XOR operation of a data block and key block, then stored in p_out.
+ *        Once key offset had reached the end, it will reset to index 0.
+ *
+ * @param p_out
+ * @param p_in1
+ * @param int1_len
+ * @param key
+ * @param key_len
+ * @param key_offset
+ */
+inline void XorBlock(void* p_out,
+                     const void* p_in1,
+                     usize len,
+                     const void* key,
+                     usize key_len,
+                     usize key_offset) {
+  const usize j = key_offset % key_len;
+  for (usize i = 0; i < len; i++) {
+    reinterpret_cast<u8*>(p_out)[i] =
+        reinterpret_cast<const u8*>(p_in1)[i] ^
+        reinterpret_cast<const u8*>(key)[(j + i) % key_len];
   }
 }
 
