c57c69f575d56792d1d19a6b47fdb11e424b6a1b
Jixun Wu <jixun.moe@gmail.com>
2022-10-22 19:25:27 +0100

    dev: use GTK3 by default

--- build-linux-docker.sh
+++ build-linux-docker.sh
@@ -15,4 +15,8 @@ cp -R AppDir/. packaging/um-desktop
 cp build/app/um-desktop packaging/um-desktop/AppRun
 
 # Do package
+if [ -z "${DEPLOY_GTK_VERSION}" ]; then
+  DEPLOY_GTK_VERSION=3
+fi
+export DEPLOY_GTK_VERSION
 linuxdeploy --appdir packaging/um-desktop --plugin gtk --output appimage
