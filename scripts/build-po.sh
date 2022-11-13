575551a7cde60bd8f3d6695eece54892c54ed0b0
Jixun Wu <jixun.moe@gmail.com>
2022-11-13 18:38:59 +0000

    build: fix locale build

--- scripts/build-po.sh
+++ scripts/build-po.sh
@@ -3,8 +3,8 @@ set -ex
 
 cd "$(git rev-parse --show-toplevel)"
 
-find locale -name '*.po' -type f -print0 \
-  | while read -r -d '' po_file; do
+find locale -name '*.po' -type f \
+  | while read -r po_file; do
     mo_file="$(echo "$po_file" | sed 's/po$/mo/')"
     msgfmt -o "$mo_file" "$po_file"
   done
