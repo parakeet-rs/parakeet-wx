af4c58b10d6c9cb412f28dce64619d3e85ca0b58
Jixun Wu <jixun.moe@gmail.com>
2022-11-25 22:12:12 +0000

    feat: serialization of general parameters.

--- src/config/AppConfigConverter.cpp
+++ src/config/AppConfigConverter.cpp
@@ -4,13 +4,50 @@ using json = nlohmann::json;
 
 namespace parakeet_wx::config {
 
+#pragma region "JSON GlueCode - Guess I have to have some :/"
+template <typename T>
+inline void get_optional_json_field(const json& j, const char* field, T& value, T defaultValue) {
+  if (j.contains(field)) {
+    j.at(field).get_to(value);
+  } else {
+    value = defaultValue;
+  }
+}
+
+template <typename T>
+inline void get_optional_json_field(const json& j, const char* field, T& value) {
+  if (j.contains(field)) {
+    j.at(field).get_to(value);
+  }
+}
+#pragma endregion
+
 void to_json(json& j, const AppConfig& config) {
-  // TODO: Implement the conversion here.
-  j = json{};
+  j = json{
+      {"general", config.desktop.general},
+
+      // TODO: Serialise decryption configuration
+  };
 }
 
 void from_json(const json& j, AppConfig& config) {
-  // TODO: Implement the conversion here.
+  config = AppConfig{};
+  get_optional_json_field(j, "general", config.desktop.general);
+
+  // TODO: Parse decryption configuration
+}
+
+void to_json(json& j, const GeneralConfig& config) {
+  j = json{
+      {"locale", config.locale},
+      {"thread_count", config.thread_count},
+  };
+}
+
+void from_json(const json& j, GeneralConfig& config) {
+  config = GeneralConfig{};
+  get_optional_json_field(j, "locale", config.locale, std::string("zh_CN"));
+  get_optional_json_field(j, "thread_count", config.thread_count, 4);
 }
 
 }  // namespace parakeet_wx::config
--- src/config/AppConfigConverter.h
+++ src/config/AppConfigConverter.h
@@ -10,4 +10,7 @@ using json = ::nlohmann::json;
 void to_json(json& j, const AppConfig& config);
 void from_json(const json& j, AppConfig& config);
 
+void to_json(json& j, const GeneralConfig& config);
+void from_json(const json& j, GeneralConfig& config);
+
 }  // namespace parakeet_wx::config
