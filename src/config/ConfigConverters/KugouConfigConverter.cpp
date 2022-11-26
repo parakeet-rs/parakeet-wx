fea02966e6060a91df10f178e03ff6f5369b2d35
Jixun Wu <jixun.moe@gmail.com>
2022-11-26 01:52:34 +0000

    fix: breakage from last commit

--- src/config/ConfigConverters/KugouConfigConverter.cpp
+++ src/config/ConfigConverters/KugouConfigConverter.cpp
@@ -2,6 +2,7 @@
 #include "VectorConverter.h"
 
 NLOHMANN_JSON_NAMESPACE_BEGIN
+using KugouConfig = parakeet_crypto::decryption::config::KugouConfig;
 namespace utils = ::parakeet_crypto::utils;
 
 void adl_serializer<KugouConfig>::to_json(json& j, const KugouConfig& data) {
@@ -11,9 +12,10 @@ void adl_serializer<KugouConfig>::to_json(json& j, const KugouConfig& data) {
 }
 
 void adl_serializer<KugouConfig>::from_json(const json& j, KugouConfig& data) {
-  KugouConfig DefFrameProcA;
-  data.slot_key_1 = j.value("slot_key_1", DefFrameProcA.slot_key_1);
-  data.v4_file_key_expansion_table = j.value("v4_file_key_expansion_table", DefFrameProcA.v4_file_key_expansion_table);
-  data.v4_slot_key_expansion_table = j.value("v4_slot_key_expansion_table", DefFrameProcA.v4_slot_key_expansion_table);
+  KugouConfig def;
+  data.slot_key_1 = j.value("slot_key_1", def.slot_key_1);
+  data.v4_file_key_expansion_table = j.value("v4_file_key_expansion_table",
+  def.v4_file_key_expansion_table); data.v4_slot_key_expansion_table =
+  j.value("v4_slot_key_expansion_table", def.v4_slot_key_expansion_table);
 }
 NLOHMANN_JSON_NAMESPACE_END
