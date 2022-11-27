ff7a9f3ed93de699dec388d42610f197857c68b3
Jixun Wu <jixun.moe@gmail.com>
2022-11-27 13:11:53 +0000

    feat: implement uint16_t array conversion

--- src/utils/json/ByteContainerConverter.hpp
+++ src/utils/json/ByteContainerConverter.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <parakeet-crypto/endian.h>
+
 #include "../BytesSerialization.h"
 
 #include <cstddef>
@@ -22,7 +24,7 @@ struct adl_serializer<std::array<uint8_t, SIZE>> {
   inline static void from_json(const json& j, std::array<uint8_t, SIZE>& data) {
     if (j.is_string()) {
       std::vector<uint8_t> temp = ::parakeet_wx::utils::DeserializeBytes(j.get<std::string>());
-      temp.resize(data.size(), 0);
+      temp.resize(SIZE, 0);
       std::copy(temp.begin(), temp.end(), data.begin());
     } else {
       std::fill(data.begin(), data.end(), 0);
@@ -33,11 +35,23 @@ struct adl_serializer<std::array<uint8_t, SIZE>> {
 template <size_t SIZE>
 struct adl_serializer<std::array<uint16_t, SIZE>> {
   inline static void to_json(json& j, const std::array<uint16_t, SIZE>& data) {
-    //
+    std::array<uint16_t, SIZE> temp(data);
+
+    std::transform(data.cbegin(), data.cend(), temp.begin(), ::parakeet_crypto::SwapHostToLittleEndian<uint16_t>);
+
+    j = ::parakeet_wx::utils::SerializeBytes(
+        std::span{reinterpret_cast<uint8_t*>(temp.data()), SIZE * sizeof(uint16_t)});
   }
 
   inline static void from_json(const json& j, std::array<uint16_t, SIZE>& data) {
-    //
+    if (j.is_string()) {
+      std::vector<uint8_t> temp = ::parakeet_wx::utils::DeserializeBytes(j.get<std::string>());
+      temp.resize(SIZE * sizeof(uint16_t), 0);
+      auto temp_ptr = std::span{reinterpret_cast<uint16_t*>(temp.data()), SIZE};
+      std::transform(temp_ptr.begin(), temp_ptr.end(), data.begin(), ::parakeet_crypto::SwapLittleEndianToHost<uint16_t>);
+    } else {
+      std::fill(data.begin(), data.end(), 0);
+    }
   }
 };
 
