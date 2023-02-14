c9a113c1e367da747efeb268acf281394e69d979
Jixun Wu <jixun.moe@gmail.com>
2023-02-14 21:44:42 +0000

    chore: update translation

--- src/utils/transform_error_text.h
+++ src/utils/transform_error_text.h
@@ -5,30 +5,30 @@
 namespace parakeet_wx
 {
 
-inline const wxString &to_string(parakeet_crypto::TransformResult result)
+inline const wxString to_string(parakeet_crypto::TransformResult result)
 {
     using parakeet_crypto::TransformResult;
 
     switch (result)
     {
     case TransformResult::OK:
-        return _("OK");
+        return wxT("OK");
     case TransformResult::ERROR_OTHER:
-        return _("ERROR_OTHER");
+        return wxT("ERROR_OTHER");
     case TransformResult::ERROR_INSUFFICIENT_OUTPUT:
-        return _("ERROR_INSUFFICIENT_OUTPUT");
+        return wxT("ERROR_INSUFFICIENT_OUTPUT");
     case TransformResult::ERROR_INVALID_FORMAT:
-        return _("ERROR_INVALID_FORMAT");
+        return wxT("ERROR_INVALID_FORMAT");
     case TransformResult::ERROR_INVALID_KEY:
-        return _("ERROR_INVALID_KEY");
+        return wxT("ERROR_INVALID_KEY");
     case TransformResult::ERROR_INSUFFICIENT_INPUT:
-        return _("ERROR_INSUFFICIENT_INPUT");
+        return wxT("ERROR_INSUFFICIENT_INPUT");
     case TransformResult::ERROR_IO_OUTPUT_UNKNOWN:
-        return _("ERROR_IO_OUTPUT_UNKNOWN");
+        return wxT("ERROR_IO_OUTPUT_UNKNOWN");
     case TransformResult::ERROR_NOT_IMPLEMENTED:
-        return _("ERROR_NOT_IMPLEMENTED");
+        return wxT("ERROR_NOT_IMPLEMENTED");
     default:
-        return _("");
+        return wxString::Format("ERROR_UNKNOWN(%d)", static_cast<int>(result));
     }
 }
 
